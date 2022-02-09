411b978c1a29a5a49909b16d8682a4138426e25b
Childishs <matthschmitz.mms@gmail.com>
2022-02-09 11:16:41 +0100

    front authent

---
+++ front_end/app/src/main/java/com/example/amphy/Serveur.java
@@ -0,0 +1,13 @@
+package com.example.amphy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Serveur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serveur);
+    }
+}
