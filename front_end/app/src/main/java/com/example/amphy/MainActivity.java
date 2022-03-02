7abdbd24a1e256fd53ae6ca7f56e4253c09620b3
Childishs <matthschmitz.mms@gmail.com>
2022-03-02 10:40:05 +0100

    authent front

--- front_end/app/src/main/java/com/example/amphy/MainActivity.java
+++ front_end/app/src/main/java/com/example/amphy/MainActivity.java
@@ -1,14 +1,115 @@
 package com.example.amphy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+    String responseStr;
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonValiderAuthentification = (Button)findViewById(R.id.buttonValiderAuthentification);
+        buttonValiderAuthentification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Appel de la fonction authentification
+                try {
+                    authentification();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        final Button buttonQuitterAuthentification = (Button)findViewById(R.id.buttonQuitterAuthentification);
+        buttonQuitterAuthentification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.finish();
+            }
+        });
     }
+
+
+
+    public void authentification() throws IOException{
+        final EditText textLogin = findViewById(R.id.editTextLogin);
+        final EditText textMdp = findViewById(R.id.editTextMdp);
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("login", textLogin.getText().toString())
+                .add("mdp",textMdp.getText().toString())
+                .build();
+
+        Request request = new Request.Builder()
+                .url("http://127.0.0.1/Amphityon/back_end/dao/UtilisateurDAO.php")
+                .post(formBody)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse( Call call, Response response) throws IOException {
+                responseStr = response.body().string();
+                Log.d("Test", responseStr);
+                if(responseStr.compareTo("flase")!=0){
+
+                    try{
+                        JSONObject etudiant = new JSONObject(responseStr);
+                        Log.d("Test",etudiant.getString("nomEtudiant") + " est  connecté");
+                        if(etudiant.getString("statut").compareTo("prof")!=0) {
+                            Intent intent = new Intent(MainActivity.this, MenuChefSalle.class);
+                            intent.putExtra("etudiant", etudiant.toString());
+                            startActivity(intent);
+                        }
+                        else {
+                            Intent intent = new Intent(MainActivity.this, MenuChefCuisine.class);
+                            intent.putExtra("etudiant", etudiant.toString());
+                            startActivity(intent);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        //Toast.makeText(MainActivity.this, "Erreur de connexion !!!! !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Log.d("Test","Login ou mot de  passe non valide !");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d("Test","erreur!!! connexion impossible");
+            }
+        });
+    }
+
+
 }
