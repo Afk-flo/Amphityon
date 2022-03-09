1d1fe358fd7a194abeb95e5c522479fc287bd138
Florian-png <florian.chauvain@gmail.com>
2022-03-09 11:41:43 +0100

    je deteste la télé

--- back_end/api/login.php
+++ back_end/api/login.php
@@ -2,8 +2,6 @@
 
 require_once('../dao/UtilisateurDAO.php');
 
-$_POST['login'] = "a";
-$_POST['mdp'] = "a";
 
 // On vérifie que les données envoyées sont bonnes 
 if(isset($_POST['login']) && !empty($_POST['login']) && isset($_POST['mdp']) && !empty($_POST['mdp'])) {
